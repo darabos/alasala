f85466f7f9af5e2c065cda218414f30d8bde380d
Hanna Gabor <gahanna999@gmail.com>
2020-09-22 16:21:10 +0200

    Stop if in range.

--- backend/heroes.py
+++ backend/heroes.py
@@ -40,13 +40,15 @@ class Hero:
     target = self.find_closest_opponent(state)
     if target is not None:
       attack = self.actions['base_attack']
-      if sqrt(self.sq_distance(target)) < attack['range']:
+      distance = sqrt(self.sq_distance(target))
+      if distance <= attack['range']:
         target.take_attack(attack)
         self.actions_in_turn = [{**attack, 'target_hero': target.id}]
       else:
         direction_x, direction_y = self.direction_to_hero(target)
-        self.x += direction_x * self.speed
-        self.y += direction_y * self.speed
+        step_size = min(self.speed, distance - attack['range'])
+        self.x += direction_x * step_size
+        self.y += direction_y * step_size
 
   def take_attack(self, attack):
     if self.loyalty >= 0:
