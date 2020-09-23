d0b3d118b8e9e2b9ee4fde29daf536dad0774b60
Daniel Darabos <darabos.daniel@gmail.com>
2020-09-23 17:32:26 +0200

    Simpler end check, sorry.

--- backend/battle_simulator.py
+++ backend/battle_simulator.py
@@ -25,20 +25,15 @@ def get_enemy_state(stage):
 
 
 def battle_is_over(turn):
-  first = True
-  for _, log_entry in turn.items():
-    if first:
-      loyal_to_player = log_entry['loyalty'] >= 0
-      first = False
-    elif loyal_to_player != (log_entry['loyalty'] >= 0):
-      return False
-  return True
+  return (
+      all(log_entry['loyalty'] <= 0 for log_entry in turn.values()) or
+      all(log_entry['loyalty'] >= 0 for log_entry in turn.values()))
 
 
 def simulate_battle(heroes, stage):
   state = get_player_state(heroes) + get_enemy_state(stage)
   turns = [{hero.id: hero.get_log_entry() for hero in state}]
-  while not battle_is_over(turns[-1]) and len(turns) < 5 * 90:
+  while not battle_is_over(turns[-1]) and len(turns) < 4 * 120:
     for hero in state:
       hero.step(stage, state)
     turns.append({hero.id: hero.get_log_entry() for hero in state})
